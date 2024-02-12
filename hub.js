065b2a80f187a9e67e1f5b6a67eca1042d8f3e43
MuHeatherHsieh <hd.reservoir@gmail.com>
2024-02-12 20:47:17 +0800

    feat: hubs added as gso nodes

--- hub.js
+++ hub.js
@@ -2,9 +2,31 @@ class Hub extends HTMLElement{
   constructor(){
     super();
 
-    const template = document.getElementById('hub');
+  }
+  connectedCallback() {
+    // create shadow DOM
+    const template = document.getElementById('hub-template');
     const shadowRoot = this.attachShadow({ mode: "open" });
     shadowRoot.appendChild(template.content.cloneNode(true));
+
+    // link stylesheet
+    const styleLink = document.createElement("link");
+    styleLink.setAttribute("rel", "stylesheet");
+    styleLink.setAttribute("href", "hubStyle.css");
+    shadowRoot.appendChild(styleLink);
+
+    const btn = shadowRoot.querySelector(".hub-btn");
+    btn.addEventListener("click", () => console.log("clicked"));
+
+    this.setPosition();
+  }
+  setPosition() {
+    const center = {x: window.innerWidth/2, y: window.innerHeight/2}
+    const x = parseInt(this.dataset.x);
+    const y = parseInt(this.dataset.y);
+    this.style.setProperty("position", "absolute");
+    this.style.setProperty("left", `${center.x + x}px`);
+    this.style.setProperty("top", `${center.y + y}px`);
   }
   
 }
--- net.js
+++ net.js
@@ -7,38 +7,46 @@ class net {
     this.center = { x: window.innerWidth/2, y: window.innerHeight/2 }
 
     this.nodes = this.randomNodes();
+    this.gssNodes = [];
     this.drawCanvas();
   };
   config = {
     nodeNum: 10,
     nodeColor: "#FFFFFF",
-    lineColor: "#FFFFFF",
+    lineColor: "khaki",
+
   }
   phy = {
     GM: 500,
-    dt: 0.05,
+    dt: 0.5,
   }
   randomNodes() {
     const r_max = Math.min(this.canvas.width, this.canvas.height)/2.5
-    const r_void = 0.3;
+    const r_void = 0.7;
     return new Array(this.config.nodeNum).fill(0).map(_ => {
       const r = (Math.random()*(1 - r_void) + r_void) * r_max;
       const theta = Math.random() * Math.PI * 2;
       const v = Math.sqrt(this.phy.GM/r);
       const dir = Math.random() > 0.5 ? 1 : (-1);
       return ({
-        x: Math.round(this.center.x + r * Math.cos(theta)),
-        y: Math.round(this.center.y + r * Math.sin(theta)),
-        v_x: Math.round(v * Math.sin(theta) * (-dir)),
-        v_y: Math.round(v * Math.cos(theta) *   dir ),
+        // x: Math.round(this.center.x + r * Math.cos(theta)),
+        // y: Math.round(this.center.y + r * Math.sin(theta)),
+        // v_x: Math.round(v * Math.sin(theta) * (-dir)),
+        // v_y: Math.round(v * Math.cos(theta) *   dir ),
+        x: this.center.x + r * Math.cos(theta),
+        y: this.center.y + r * Math.sin(theta),
+        v_x: v * Math.sin(theta) * (-dir),
+        v_y: v * Math.cos(theta) *   dir ,
       })
     })
   }
 
   drawCanvas() {
     const nodes = this.nodes;
+    const gsss = this.gssNodes;
     const canvas = this.canvas;
     const ctx = this.ctx;
+
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = "#FF0000";
     // ctx.fillRect(this.center.x, this.center.y, 3, 3);
@@ -51,9 +59,9 @@ class net {
           // calculate distance
           const dx = node.x - edon.x;
           const dy = node.y - edon.y;
-          const lineW = 6000 / (dx * dx + dy * dy);
+          const lineW = 5000 / (dx * dx + dy * dy);
           if(lineW > 0.1){
-            if(lineW > 2) ctx.lineWidth = 2;
+            if(lineW > 1.5) ctx.lineWidth = 1.5;
             else ctx.lineWidth = lineW;
 
             // draw the line
@@ -64,7 +72,23 @@ class net {
           }
         }
       })
+      gsss.forEach(gss => {
+        // calculate distance
+        // console.log(gss.x);
+        const dx = node.x - gss.x - this.center.x;
+        const dy = node.y - gss.y - this.center.y;
+        const lineW = 5000 / (dx * dx + dy * dy);
+        if(lineW > 0.1){
+          if(lineW > 1.5) ctx.lineWidth = 1.5;
+          else ctx.lineWidth = lineW;
 
+          // draw the line
+          ctx.beginPath();
+          ctx.moveTo(node.x, node.y);
+          ctx.lineTo(gss.x + this.center.x, gss.y + this.center.y);
+          ctx.stroke();
+        }
+      })
       // move a little bit
       const dt = this.phy.dt;
       const dx = node.x - this.center.x;
@@ -74,12 +98,11 @@ class net {
       node.y = node.y + node.v_y * dt;
       node.v_x = node.v_x + (this.center.x - node.x) * da_r * dt;
       node.v_y = node.v_y + (this.center.y - node.y) * da_r * dt;
-      // node.x = Math.round(node.x + node.v_x);
-      // node.y = Math.round(node.y + node.v_y);
     })
     window.requestAnimationFrame(() => this.drawCanvas.call(this));
   }
-  
+  appendGss(newNode) {
+    this.gssNodes.push(newNode);
+  }
 }
 
-// new net(document.getElementById('bg-net'));
